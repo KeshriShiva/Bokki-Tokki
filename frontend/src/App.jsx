d9462a4297cb61f2ed4bc0e60bf3e340ff68c7e8
Shiva Keshri <shivakeshri07@gmail.com>
2026-04-21 20:04:29 +0530

    Switch to HashRouter to fix Vercel static routing completely

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, Component, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
